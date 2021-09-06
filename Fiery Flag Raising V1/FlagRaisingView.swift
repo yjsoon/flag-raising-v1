cf1b50cca37436c678d8d6d9f6d8ce0e9bcf9751
YJ Soon <yjsoon@tinkertanker.com>
2021-09-06 10:29:37 +0800

    Finished Flag Raising View

---
+++ Fiery Flag Raising V1/FlagRaisingView.swift
@@ -0,0 +1,40 @@
+//
+//  FlagRaisingView.swift
+//  Fiery Flag Raising V1
+//
+//  Created by YJ Soon on 6/9/21.
+//
+
+import SwiftUI
+
+struct FlagRaisingView: View {
+    
+    @State var offset: CGFloat = 20
+    
+    var body: some View {
+        VStack {
+            HStack(alignment: .bottom, spacing: -10) {
+                Rectangle()
+                    .frame(width: 10)
+                Text("🇸🇬")
+                    .font(.system(size: 100))
+                    .offset(y: offset)
+            }
+            Button("Raise Flag") {
+                offset -= 20
+            }
+            .padding()
+            Button("Reset Flag") {
+                offset = 20
+            }
+            .padding()
+        }
+        
+    }
+}
+
+struct FlagRaisingView_Previews: PreviewProvider {
+    static var previews: some View {
+        FlagRaisingView()
+    }
+}
